fa8416726f974ac9fedd1af43c4f7066cb5aebf8
jbielak <justyna_bielak@outlook.com>
2018-05-28 16:48:31 +0200

    feat: develop free and paid flavors

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,10 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.jbielak.javajokes.Joker;
-
 
 public class MainActivity extends AppCompatActivity {
 
